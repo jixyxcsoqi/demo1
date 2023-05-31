8d51075a69e60807e4bbe8021c69c9f1297a1160
prosuoqi <91357188+jixyxcsoqi@users.noreply.github.com>
2023-05-31 14:37:50 +0800

    Add files via upload

---
+++ helloworld/src/main/java/com/example/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 即兴小索奇
+ * @version 1.0
+ * @date 2023/5/31  13:51
+ * @description
+ */
+@RestController
+@RequestMapping({"/hello"})
+public class HelloController {
+    @GetMapping("")
+    public String hello(){
+        System.out.println("helloWorld");
+        return "OK";
+    }
+}

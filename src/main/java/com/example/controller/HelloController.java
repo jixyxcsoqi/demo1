3c09e9af5b301a5e07ef0dac2f4f3299514bc7f4
itcast <hello@itcast.cn>
2023-06-01 21:11:20 +0800

    test314

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -16,6 +16,6 @@ public class HelloController {
     @GetMapping("")
     public String hello(){
         System.out.println("helloWorldTest222");
-        return "OK22";
+        return "OK222";
     }
 }

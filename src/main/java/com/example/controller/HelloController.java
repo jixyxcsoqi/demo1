053fc72eb8ab43665e8a1374af32e96a1d35e182
itcast <hello@itcast.cn>
2023-06-01 20:39:18 +0800

    test

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("")
     public String hello(){
-        System.out.println("helloWorldTest");
+        System.out.println("helloWorldTest1");
         return "OK";
     }
 }
